f63af9d717800b9482f649ae13efd71fc0ebba32
Christian Allred <callred@ntdev.microsoft.com>
2019-02-14 16:13:04 -0800

    Update RunTests.bat

--- scripts/RunTests.bat
+++ scripts/RunTests.bat
@@ -5,7 +5,6 @@ IF "%1"=="" (SET "Configuration=Debug") ELSE (SET "Configuration=%1")
 
 set RESULT=0
 
-%VFS_OUTPUTDIR%\GVFS.UnitTests.Windows\bin\x64\%Configuration%\GVFS.UnitTests.Windows.exe  || set RESULT=1
-dotnet %VFS_OUTPUTDIR%\GVFS.UnitTests\bin\x64\%Configuration%\netcoreapp2.1\GVFS.UnitTests.dll  || set RESULT=1
+%VFS_OUTPUTDIR%\ProjectedFSLib.Managed.Test\bin\x64\Release\net461\ProjectedFSLib.Managed.Test.exe --params ProviderExe=%VFS_OUTPUTDIR%\SimpleProviderManaged\bin\x64\Release\SimpleProviderManaged.exe  || set RESULT=1
 
 exit /b %RESULT%
