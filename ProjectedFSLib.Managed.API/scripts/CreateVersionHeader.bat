ec1f116c9ca14233e6c3f5d55c6320a089b1bf31
Erik Mavrinac <erikmav@microsoft.com>
2019-11-25 19:00:44 -0800

    Move BuildOutput under the enlistment

--- ProjectedFSLib.Managed.API/scripts/CreateVersionHeader.bat
+++ ProjectedFSLib.Managed.API/scripts/CreateVersionHeader.bat
@@ -4,13 +4,13 @@ set ProjectName=%1
 set VersionString=%2
 set SolutionDir=%3
 
-mkdir %SolutionDir%\..\BuildOutput
-mkdir %SolutionDir%\..\BuildOutput\%ProjectName%
+mkdir %SolutionDir%\BuildOutput
+mkdir %SolutionDir%\BuildOutput\%ProjectName%
 
 set comma_version_string=%VersionString%
 set comma_version_string=%comma_version_string:.=,%
 
-echo #define PRJLIB_FILE_VERSION %comma_version_string% > %SolutionDir%\..\BuildOutput\%ProjectName%\VersionHeader.h
-echo #define PRJLIB_FILE_VERSION_STRING "%VersionString%" >> %SolutionDir%\..\BuildOutput\%ProjectName%\VersionHeader.h
-echo #define PRJLIB_PRODUCT_VERSION %comma_version_string% >> %SolutionDir%\..\BuildOutput\%ProjectName%\VersionHeader.h
-echo #define PRJLIB_PRODUCT_VERSION_STRING "%VersionString%" >> %SolutionDir%\..\BuildOutput\%ProjectName%\VersionHeader.h
+echo #define PRJLIB_FILE_VERSION %comma_version_string% > %SolutionDir%\BuildOutput\%ProjectName%\VersionHeader.h
+echo #define PRJLIB_FILE_VERSION_STRING "%VersionString%" >> %SolutionDir%\BuildOutput\%ProjectName%\VersionHeader.h
+echo #define PRJLIB_PRODUCT_VERSION %comma_version_string% >> %SolutionDir%\BuildOutput\%ProjectName%\VersionHeader.h
+echo #define PRJLIB_PRODUCT_VERSION_STRING "%VersionString%" >> %SolutionDir%\BuildOutput\%ProjectName%\VersionHeader.h
